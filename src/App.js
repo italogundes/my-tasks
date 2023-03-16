915b6a954fce76d7c2025e2595c46af505835739
Italo Mateus Mesquita Costa <italo.costa@benner.com.br>
2023-03-16 16:55:24 -0300

    atualizações e finalização da primeira versão do projeto

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import TableTasks from './components/TableTasks';
+import {BsPlusSquare} from 'react-icons/bs';
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
+  const [taskList, setTaskList] = useState([]);
   const [task, setTask] = useState('');
   const inputTask = useRef();
 
   useEffect(() => {
-    console.log(tasks);
-    localStorage.setItem('tasks', tasks);
-  },[tasks])
+    const tasksLocal = JSON.parse(localStorage.getItem('tasks'));
+    if(tasksLocal.length !== 0) {
+    setTaskList(tasksLocal);
+    }
+  }, []);
+
+  useEffect(() => {
+    if(taskList.length !== 0) {
+      localStorage.setItem('tasks', JSON.stringify(taskList));
+    }
+  },[taskList]);
   
 
   function handleSubmit(event) {
     event.preventDefault();
-    setTasks([...tasks, task]);
+    const id = taskList.length + 1;
+    setTaskList([...taskList, {
+      id: id,
+      task: task,
+      complete: false
+    }]);
     setTask('');
     inputTask.current.focus();
   }
 
-  function changeInput({target}){
-    setTask(target.value);
-  }
+  /*function changeInput({target}){
+    const {id, value} = target;
+    setTask({...task, [id]:value}); //outra forma de pegar valor de um input
+  }*/
 
   
   return (
     <div className="App">
       <h1>my tasks</h1>
       <form onSubmit={handleSubmit} className='input-content'>
-        <input ref={inputTask} onChange={changeInput} value={task} type="text" placeholder='Nova Tarefa'/> <button className='btn-adicionar'>+</button>
+        <input 
+        ref={inputTask} 
+        id='descricao' 
+        type="text" 
+        value={task} 
+        onChange={(e) => setTask(e.target.value)} 
+        placeholder='Nova Tarefa'/> 
+        <button className='btn-adicionar'><BsPlusSquare/></button>
       </form>
-      <TableTasks tasks={tasks}/>
+      <TableTasks taskList={taskList} setTaskList={setTaskList}/>
     </div>
   );
 }
